d8dfb0a115d20400ea2aa8f866b6a924c6734026
Ubuntu <ayush@parspec.io>
2023-06-26 12:10:56 +0000

    Added shell script for cleanup

---
+++ cleanup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Remove PDF files from input directory
+rm pdf/input/*.pdf
+
+# Remove PDF files from output directory
+rm pdf/output/*.pdf
+
+# Remove HTML files from directory
+rm html/*.html
